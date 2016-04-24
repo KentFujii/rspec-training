f7026df775f5b5fea047791e62af73c18a205ded
Kent Fujii <kent.where.the.light.is@gmail.com>
2016-04-25 00:57:34 +0900

    WIP chapter5

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 describe Contact do
 
-  it "is valid with a firstname, lastname and email" do
-    contact = Contact.new(
-      firstname: 'Aaron',
-      lastname: 'Sumner',
-      email: 'tester@example.com')
-    expect(contact).to be_valid
-  end
-
   it "is invalid without a firstname" do
     contact = Contact.new(firstname: nil)
     contact.valid?
@@ -28,42 +20,10 @@ describe Contact do
     expect(contact.errors[:email]).to include("can't be blank")
   end
 
-  it "is invalid with a duplicate email address" do
-    Contact.create(
-      firstname: 'Joe', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
-    contact = Contact.new(
-      firstname: 'Joe', lastname: 'Tester',
-      email: 'tester@example.com'
-    )
-    contact.valid?
-    expect(contact.errors[:email]).to include("has already been taken")
-  end
-
   it "returns a contact's full name as a string" do
     contact = Contact.new(firstname: 'John', lastname: 'Doe',
       email: 'johndoe@example.com')
     expect(contact.name).to eq 'John Doe'
-  end
-
-  it "returns a sorted array of results that match" do
-    smith = Contact.create(
-      firstname: 'John',
-      lastname: 'Smith',
-      email: 'jsmith@example.com'
-    )
-    jones = Contact.create(
-      firstname: 'Tim',
-      lastname: 'Jones',
-      email: 'tjones@example.com'
-    )
-    johnson = Contact.create(
-      firstname: 'John',
-      lastname: 'Johnson',
-      email: 'jjohnson@example.com'
-    )
-    expect(Contact.by_letter('J')).to eq [johnson, jones]
   end
 
   it 'omits results that do not match' do
@@ -102,11 +62,6 @@ describe Contact do
         lastname: 'Johnson',
         email: 'jjohnson@example.com'
       )
-    end
-    context 'matching letters' do
-      it 'returns a sorted array of results that match' do
-        expect(Contact.by_letter('J')).to eq [@johnson, @jones]
-      end
     end
 
     context 'non-matching letters' do
